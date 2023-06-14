1bd19b90734e713b90a349b76c817d132b950690
abu-jafor <abujaforwd@yahoo.com>
2023-06-14 18:30:14 +0600

    some comment

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// First Comment
 app.get("/", (req, res) => {
   res.send("Hello Assingment-12 and last assingment");
 });
